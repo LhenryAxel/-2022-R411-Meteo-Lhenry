010adf92c3065d516e62a2f09b9fc46042ca1693
LhenryAxel <axel.lhenry@iut-dijon.u-bourgogne.fr>
2023-03-28 09:54:53 +0200

    Creation attribut correspondant aux controles

--- LHENRYAxelMEteo/app/src/main/java/com/example/lhenryaxelmeteo/MainActivity.java
+++ LHENRYAxelMEteo/app/src/main/java/com/example/lhenryaxelmeteo/MainActivity.java
@@ -4,10 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button search;
+    private ImageView search;
+    private ImageView gps;
+    private ImageView next;
+    private ImageView prev;
+    private EditText text;
+    private TextView coord;
+
 
 
 
@@ -15,5 +24,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        search = findViewById(R.id.search);
+        gps = findViewById(R.id.gps);
+        next = findViewById(R.id.next);
+        prev = findViewById(R.id.prev);
+        text = findViewById(R.id.editTextTextPersonName2);
+        coord = findViewById(R.id.coord);
     }
 }
