e15aa3676fe6141fa6f229d7f05801ddda8e9423
LhenryAxel <axel.lhenry@iut-dijon.u-bourgogne.fr>
2023-03-28 09:44:15 +0200

    Début phase IHM

--- LHENRYAxelMEteo/app/src/main/java/com/example/lhenryaxelmeteo/MainActivity.java
+++ LHENRYAxelMEteo/app/src/main/java/com/example/lhenryaxelmeteo/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.lhenryaxelmeteo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button search;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
